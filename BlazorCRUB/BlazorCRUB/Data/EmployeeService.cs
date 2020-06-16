942708700c2a1e957c7a55c10fa9f74782bb0ecb
diego10mf <47178119+diego10mf@users.noreply.github.com>
2020-06-16 17:02:37 +0200

    cambio final

---
+++ BlazorCRUB/BlazorCRUB/Data/EmployeeService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUB.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext _Db;
+
+        public EmployeeService(ApplicationDbContext db)
+        {
+            _Db = db;
+        }
+
+        public List<EmployeeInfo> GetEmployee()
+        {
+            var empList = _Db.Employees.ToList();
+            return empList;
+        }
+
+        public string Create(EmployeeInfo objemployee)
+        {
+            _Db.Employees.Add(objemployee);
+            _Db.SaveChanges();
+            return "Save Successfully";
+        }
+
+        public EmployeeInfo GetEmployeeById(int id)
+        {
+           EmployeeInfo employee = _Db.Employees.FirstOrDefault( s => s.EmployeeId == id);
+            return employee;
+        }
+
+        public string UpdateEmployee(EmployeeInfo objemployee)
+        {
+            _Db.Employees.Update(objemployee);
+            _Db.SaveChanges();
+            return "Update Successfully";
+        }
+
+        public string DeleteEmployee(EmployeeInfo objemployee)
+        {
+            _Db.Remove(objemployee);
+            _Db.SaveChanges();
+            return "Delete Successfully";
+        }
+    }
+}
